ea11bd3fb41514d3aa6ebc154ff72f2a2f271837
Luke <ubuntu@ip-172-31-38-73.us-west-2.compute.internal>
2014-10-11 09:10:14 +0000

    Added node+express api

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+app.get('/api', function(req, res) {
+    res.send("API")
+});
