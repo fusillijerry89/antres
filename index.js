e0fb63134ac890a3c3202a0d4ec433ee197ed231
Luke <ubuntu@ip-172-31-38-73.us-west-2.compute.internal>
2014-10-11 09:31:25 +0000

    Fixed api error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
 
-app.get('/api:var', function(req, res, next) {
+app.get('/api/:var', function(req, res, next) {
     res.send(req.params.id);
     return next();
 });
